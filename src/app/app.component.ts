89debfe1ba9a1215d688b5d9a002d4d2a834d3b7
annailvyadne <annailvyadne@gmail.com>
2025-01-30 23:54:35 +0800

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'events-website';
+
+    hideHeader = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url;
+      this.hideHeader = currentUrl.startsWith('/admin') || currentUrl === '/login' || currentUrl === '/signup';
+    });
+  }
 }
+
